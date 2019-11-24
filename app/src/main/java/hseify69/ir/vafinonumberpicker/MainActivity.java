71758b2809c3b3aea53d21d647a26127fed35009
Matin <matin.ashtiani@gmail.com>
2019-11-24 18:03:35 +0330

    change lib min sdk number

--- app/src/main/java/hseify69/ir/vafinonumberpicker/MainActivity.java
+++ app/src/main/java/hseify69/ir/vafinonumberpicker/MainActivity.java
@@ -3,6 +3,8 @@ package hseify69.ir.vafinonumberpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import hseify69.ir.numpad.Numpad;
@@ -15,12 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Numpad numpad = findViewById(R.id.numpad);
+        TextView txtInput1 = findViewById(R.id.txtInput1);
+        TextView txtInput2 = findViewById(R.id.txtInput2);
+        TextView txtInput3 = findViewById(R.id.txtInput3);
 
         numpad.setMaxLength(10);
 
         numpad.setOnChangeEntered(new Numpad.OnChangeEntered() {
             @Override
             public void onChange(String wholeEntered) {
+            }
+        });
+        txtInput1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
