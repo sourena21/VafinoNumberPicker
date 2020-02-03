0c8326be34a571f241c4152ddebec7febf56c328
Matin <matin.ashtiani@gmail.com>
2020-02-03 14:24:56 +0330

    add ability to change keyboard input type

--- app/src/main/java/hseify69/ir/vafinonumberpicker/MainActivity.java
+++ app/src/main/java/hseify69/ir/vafinonumberpicker/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        keyboard = findViewById(R.id.keyboard);
+//        keyboard = findViewById(R.id.keyboard);
     }
 
     private void setBackgroundResource(TextView textView, int src) {
