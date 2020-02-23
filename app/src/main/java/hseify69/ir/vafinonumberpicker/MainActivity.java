ce2a12fb671c11171ee45ce19c4c943bdbe8a528
Matin <matin.ashtiani@gmail.com>
2020-02-23 15:55:43 +0330

    solve changing focus bug for third party multiple inputs

--- app/src/main/java/hseify69/ir/vafinonumberpicker/MainActivity.java
+++ app/src/main/java/hseify69/ir/vafinonumberpicker/MainActivity.java
@@ -3,8 +3,12 @@ package hseify69.ir.vafinonumberpicker;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,56 +20,43 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends Activity {
 
-    //    VafinoKeyboard keyboard;
-//    TextView edtName, edtFamily, txtSelected;
-    KeyboardPersianDatePicker datePicker;
-    Button btnSubmitDate;
+    VafinoKeyboard keyboard;
+    EditText edtName, edtFamily;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        datePicker = findViewById(R.id.keyboard);
-        btnSubmitDate = findViewById(R.id.btnSubmitDate);
+        keyboard = findViewById(R.id.keyboard);
+        edtName = findViewById(R.id.edtName);
+        edtFamily = findViewById(R.id.edtFamily);
 
-        btnSubmitDate.setOnClickListener(new View.OnClickListener() {
+        edtName.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        edtName.setTextIsSelectable(true);
+        edtFamily.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        edtFamily.setTextIsSelectable(true);
+
+        edtName.requestFocus();
+        keyboard.setInput(edtName);
+
+        edtName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "" + datePicker.getSelectedDate(), Toast.LENGTH_SHORT).show();
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    keyboard.setInput(edtName);
+                }
             }
         });
-//        keyboard = findViewById(R.id.keyboard);
-//        edtName = findViewById(R.id.edtName);
-//        edtFamily = findViewById(R.id.edtFamily);
-//
-//        edtName.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                txtSelected = edtName;
-//                keyboard.setEntered(txtSelected.getText().toString());
-//            }
-//        });
-//        edtFamily.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                txtSelected = edtFamily;
-//                keyboard.setEntered(txtSelected.getText().toString());
-//            }
-//        });
-//        keyboard.setOnChangeEntered(new VafinoKeyboard.OnChangeEntered() {
-//            @Override
-//            public void onChange(String wholeEntered) {
-//                showEntered(wholeEntered);
-//            }
-//        });
+        edtFamily.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    keyboard.setInput(edtFamily);
+                }
+            }
+        });
     }
-
-//    private void showEntered(String entered) {
-//        if (txtSelected != null) {
-//            txtSelected.setText(entered);
-//        }
-//    }
 
     @Override
     protected void attachBaseContext(Context newBase) {
